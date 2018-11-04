385da0ff18ae9ee658d1229bdec6a04139af760d
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2018-11-04 14:56:38 +0800

    优化 ChainedSelect input

--- src/Forms/Controls/ChainedInput.php
+++ src/Forms/Controls/ChainedInput.php
@@ -6,80 +6,81 @@ use Nette\Forms\Controls\BaseControl;
 
 /**
  * 链式选择输入
- *
- * @deprecated
  */
 class ChainedInput extends BaseControl
 {
 
-	private $settings = [];
-	private $fields = [];
+    private $settings = [];
+    private $fields = [];
 
-	/**
-	 * @param  string|object $label    Html 标签
-	 * @param  array         $settings TinyMce 设置
-	 * @param  array         $fields   TinyMce 设置
-	 */
-	public function __construct( $label = null, $settings = [], $fields = [] )
-	{
-		parent::__construct( $label );
-		$this->settings = $settings;
-		$this->fields   = $fields;
-	}
+    /**
+     * @param  string|object $label    Html 标签
+     * @param  array         $settings TinyMce 设置
+     * @param  array         $fields   TinyMce 设置
+     */
+    public function __construct($label = null, $settings = [], $fields = [])
+    {
+        parent::__construct($label);
+        $this->settings = $settings;
+        $this->fields   = $fields;
+
+        $this->setOption('type', 'chained');
+    }
 
 
-	/**
-	 *  生成 html
-	 *
-	 * @return string
-	 */
-	public function getControl()
-	{
+    /**
+     *  生成 html
+     *
+     * @return string
+     */
+    public function getControl()
+    {
 
-		$id            = $this->getHtmlId();
-		$name          = $this->getHtmlName();
-		$settings      = $this->settings;
-		$fields        = $this->fields;
-		$default_value = $this->value ? $this->value : [];
+        $id            = $this->getHtmlId();
+        $name          = $this->getHtmlName();
+        $settings      = $this->settings;
+        $fields        = $this->fields;
+        $default_value = $this->value ? $this->value : [];
 
-		if ( function_exists( 'wp_enqueue_script' ) ) {
-			wp_enqueue_script( 'frm-chained' );
-		}
+        if (function_exists('wp_enqueue_script')) {
+            wp_enqueue_script('frm-chained');
+        }
 
-		$js_fields = '["' . implode( '","', $fields ) . '"]';
+        $settings_default = [
+            'selects'    => $fields,
+            'emptyStyle' => 'none',
+        ];
 
-		$html = '<div id="' . $id . '" class="input-group frm-chained">';
+        $settings = array_merge($settings_default, $settings);
 
-		$i = 0;
-		foreach ( $fields as $field ) {
-			$html .= '<select name="' . $name . '" class="form-control ' . $field . '" data-value="' . $default_value[ $i ] . '"></select>';
-			$i ++;
-		}
+        $html = '<div id="' . $id . '" class="input-group frm-chained">';
 
-		$html .= '</div>';
+        $i = 0;
+        foreach ($fields as $field) {
+            $html .= '<select name="' . $name . '" class="form-control ' . $field . '" data-value="' . $default_value[ $i ] . '"></select>';
+            $i++;
+        }
 
-		$html .= "<script>
+        $html .= '</div>';
+
+        $html .= "<script>
 		    jQuery(document).ready(function($){
-		        $('#" . $id . "').cxSelect({
-				  url: '" . $settings[ 'url' ] . "',
-				  selects: $js_fields,
-				  emptyStyle: 'none'
-				});
+		        $('#$id').cxSelect(" . json_encode($settings) . ");
 		    });
 		</script>";
 
-		return $html;
-	}
+        return $html;
+    }
 
 
-	/**
-	 * 获取 HTML 名称
-	 *
-	 * @return mixed
-	 */
-	public function getHtmlName()
-	{
-		return parent::getHtmlName() . '[]';
-	}
+    /**
+     * 获取 HTML 名称
+     *
+     * @return mixed
+     */
+    public function getHtmlName()
+    {
+        return parent::getHtmlName() . '[]';
+    }
 
 }
