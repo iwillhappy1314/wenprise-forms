e9dfdb490049e8a96b2b50b0b28581bf9842788f
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2021-01-12 15:34:41 +0800

    添加上传进度条

--- frontend/assets/scripts/modules/ajax-uploader.js
+++ frontend/assets/scripts/modules/ajax-uploader.js
@@ -2,93 +2,92 @@
 
 jQuery(document).ready(function($) {
 
-    var wprs_uploader = $('.js-uploader');
-    var wprs_msg_el = $('.js-uploader-message');
-    var options = wprs_uploader.data('settings');
+    var wprs_uploader = $('.js-uploader'),
+        options = wprs_uploader.data('settings'),
 
-    var settings = {
-        url             : $(this).find('.rs-uploader__shadow').data('url'),
-        type            : 'POST',
-        dataType        : 'json',
-        maxFileSize     : 2000000,
-        auto            : true,
-        queue           : false,
-        multiple        : ($(this).data('multiple') === true),
-        onBeforeUpload  : function() {
-            wprs_msg_el.empty();
-        },
-        onDragEnter     : function() {
-            $(this).addClass('active');
-        },
-        onDragLeave     : function() {
-            $(this).removeClass('active');
-        },
-        onUploadSuccess : function(id, data) {
-            var name = $(this).data('name'),
-                is_multiple = ($(this).data('multiple') === true),
-                button = '<button type="button" class="rs-uploader__close" data-value=' + data.id +
-                    '><svg t="1575261098184" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3639" width="12" height="12"><path d="M49.6 158.4l104-108.8 358.4 352 356.8-352 105.6 105.6-352 356.8 352 355.2-102.4 107.2L512 620.8 155.2 974.4l-105.6-105.6L406.4 512z" p-id="3640" fill="#ffffff"></path></svg></button>',
-                thumb = '<img src="' + data.thumb + '" alt="Thumbnail">';
+        settings = {
+            url             : $(this).find('.rs-uploader__shadow').data('url'),
+            type            : 'POST',
+            dataType        : 'json',
+            maxFileSize     : 2000000,
+            auto            : true,
+            queue           : false,
+            multiple        : ($(this).data('multiple') === true),
+            onBeforeUpload  : function() {
+                this.find('.js-uploader-message').empty();
+            },
+            onDragEnter     : function() {
+                this.addClass('active');
+            },
+            onDragLeave     : function() {
+                this.removeClass('active');
+            },
+            onUploadSuccess : function(id, data) {
+                var name = $(this).data('name'),
+                    is_multiple = ($(this).data('multiple') === true),
+                    button = '<button type="button" class="rs-uploader__close" data-value=' + data.id +
+                        '><svg t="1575261098184" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3639" width="12" height="12"><path d="M49.6 158.4l104-108.8 358.4 352 356.8-352 105.6 105.6-352 356.8 352 355.2-102.4 107.2L512 620.8 155.2 974.4l-105.6-105.6L406.4 512z" p-id="3640" fill="#ffffff"></path></svg></button>',
+                    thumb = '<img src="' + data.thumb + '" alt="Thumbnail">';
 
-            if (!data.thumb) {
-                thumb = '<svg t="1540194811704" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2457" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><defs><style type="text/css"></style></defs><path d="M768 426.666667H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333zM341.333333 324.266667h170.666667a17.066667 17.066667 0 1 0 0-34.133334H341.333333a17.066667 17.066667 0 1 0 0 34.133334zM768 563.2H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333zM768 699.733333H341.333333a17.066667 17.066667 0 1 0 0 34.133334h426.666667a17.066667 17.066667 0 1 0 0-34.133334zM768 836.266667H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333z" p-id="2458" fill="#666666"></path><path d="M836.266667 248.9344V0H102.4v938.666667h85.333333v85.333333h733.866667V334.267733l-85.333333-85.333333z m-153.6-105.335467l153.6 153.6L863.3344 324.266667H682.666667V143.598933zM136.533333 904.533333V34.133333h665.6v180.667734L672.6656 85.333333H187.733333v819.2H136.533333z m85.333334 85.333334V119.466667h426.666666v238.933333h238.933334v631.466667H221.866667z" p-id="2459" fill="#666666"></path></svg><br>' +
-                    data.title;
-            }
-
-            console.log(this);
-
-            this.
-                find('input:text').
-                filter(function() { return this.value === ''; }).
-                remove();
-
-            if (!is_multiple) {
-                this.find('.rs-uploader__text').hide();
-                this.find('.rs-uploader__button').hide();
+                if (!data.thumb) {
+                    thumb = '<svg t="1540194811704" class="icon" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2457" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><defs><style type="text/css"></style></defs><path d="M768 426.666667H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333zM341.333333 324.266667h170.666667a17.066667 17.066667 0 1 0 0-34.133334H341.333333a17.066667 17.066667 0 1 0 0 34.133334zM768 563.2H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333zM768 699.733333H341.333333a17.066667 17.066667 0 1 0 0 34.133334h426.666667a17.066667 17.066667 0 1 0 0-34.133334zM768 836.266667H341.333333a17.066667 17.066667 0 1 0 0 34.133333h426.666667a17.066667 17.066667 0 1 0 0-34.133333z" p-id="2458" fill="#666666"></path><path d="M836.266667 248.9344V0H102.4v938.666667h85.333333v85.333333h733.866667V334.267733l-85.333333-85.333333z m-153.6-105.335467l153.6 153.6L863.3344 324.266667H682.666667V143.598933zM136.533333 904.533333V34.133333h665.6v180.667734L672.6656 85.333333H187.733333v819.2H136.533333z m85.333334 85.333334V119.466667h426.666666v238.933333h238.933334v631.466667H221.866667z" p-id="2459" fill="#666666"></path></svg><br>' +
+                        data.title;
+                }
 
                 this.
-                    find('.rs-uploader__value').
-                    empty().
-                    append('<input type="hidden" name="' + name + '" value="' + data.id + '">');
+                    find('input:text').
+                    filter(function() { return this.value === ''; }).
+                    remove();
 
-                this.
-                    find('.rs-uploader__preview').
-                    empty().
-                    show().
-                    append('<div class="rs-uploader__thumbnail">' + button + thumb + '</div>');
-            } else {
+                if (!is_multiple) {
+                    this.find('.rs-uploader__text').hide();
+                    this.find('.rs-uploader__button').hide();
 
-                this.
-                    find('.rs-uploader__value').
-                    append('<input type="hidden" name="' + name + '" value="' + data.id + '">');
+                    this.
+                        find('.rs-uploader__value').
+                        empty().
+                        append('<input type="hidden" name="' + name + '" value="' + data.id + '">');
 
-                this.
-                    find('.rs-uploader__preview').
-                    show().
-                    append('<div class="rs-uploader__thumbnail">' + button + thumb + '</div>');
-            }
+                    this.
+                        find('.rs-uploader__preview').
+                        empty().
+                        show().
+                        append('<div class="rs-uploader__thumbnail">' + button + thumb + '</div>');
+                } else {
 
-        },
-        onUploadError   : function(id, xhr, status, errorThrown) {
-        },
-        onUploadComplete: function(id) {
-        },
-        onUploadCanceled: function(id) {
-            wprs_msg_el.html('您已取消上传文件');
-        },
-        onUploadProgress: function(id, percent) {
-            wprs_msg_el.addClass('js-progress').css('width', percent + '%');
-        },
-        onFileTypeError : function(file) {
-            wprs_msg_el.html('文件类型错误');
-        },
-        onFileSizeError : function(file) {
-            wprs_msg_el.html('文件尺寸超过限制');
-        },
-        onFileExtError  : function(file) {
-            wprs_msg_el.html('文件类型错误');
-        },
-    };
+                    this.
+                        find('.rs-uploader__value').
+                        append('<input type="hidden" name="' + name + '" value="' + data.id + '">');
+
+                    this.
+                        find('.rs-uploader__preview').
+                        show().
+                        append('<div class="rs-uploader__thumbnail">' + button + thumb + '</div>');
+                }
+
+            },
+            onUploadError   : function(id, xhr, status, errorThrown) {
+                this.find('.js-uploader-message').html('上传错误，请重试。');
+            },
+            onUploadComplete: function(id) {
+                this.find('.js-progress').remove();
+            },
+            onUploadCanceled: function(id) {
+                this.find('.js-uploader-message').html('您已取消上传文件');
+            },
+            onUploadProgress: function(id, percent) {
+                this.find('.js-uploader-message').html($('<div class="js-progress">').css('width', percent + '%'));
+            },
+            onFileTypeError : function(file) {
+                this.find('.js-uploader-message').html('文件类型错误');
+            },
+            onFileSizeError : function(file) {
+                this.find('.js-uploader-message').html('文件尺寸超过限制');
+            },
+            onFileExtError  : function(file) {
+                this.find('.js-uploader-message').html('文件类型错误');
+            },
+        };
 
     $.extend(settings, options);
 
