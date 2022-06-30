5b0c99e437d9d42f79c14cc0d9b07888f82b6d71
Amos Lee <iwillhappy1314@gmail.com>
2022-07-01 03:10:51 +0800

    在后台引入表单样式

--- src/Init.php
+++ src/Init.php
@@ -14,6 +14,7 @@ class Init
 
         add_action('init', [$this, 'register_locals']);
         add_action('wp_enqueue_scripts', [$this, 'register_assets']);
+        add_action('admin_enqueue_scripts', [$this, 'register_assets']);
     }
 
 
