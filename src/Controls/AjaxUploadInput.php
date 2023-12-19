9ae1d01fc77b295a9db4314b4e4e3cef29c517ca
Amos Lee <iwillhappy1314@gmail.com>
2023-12-19 16:20:05 +0800

    Ajax uploader PHP 预览添加文件名

--- src/Controls/AjaxUploadInput.php
+++ src/Controls/AjaxUploadInput.php
@@ -159,7 +159,11 @@ class AjaxUploadInput extends BaseControl
      */
     public function getPreview($value): string
     {
+        if (empty($value)) {
+            return '';
+        }
 
+        $attachment = get_post($value);
         $close_icon = '<svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="12" height="12"><path d="M49.6 158.4l104-108.8 358.4 352 356.8-352 105.6 105.6-352 356.8 352 355.2-102.4 107.2L512 620.8 155.2 974.4l-105.6-105.6L406.4 512z" p-id="3640" fill="#ffffff"></path></svg>';
 
         if (function_exists('wp_get_attachment_thumb_url')) {
@@ -180,7 +184,10 @@ class AjaxUploadInput extends BaseControl
         $image = Html::el('img')
                      ->setAttribute('src', $thumb);
 
-        $preview->addHtml($button . $image);
+        $file_name = Html::el('div')
+                         ->setText($attachment->post_title);
+
+        $preview->addHtml($button . $image . $file_name);
 
         return $preview;
 
